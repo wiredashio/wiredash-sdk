04ea0bb8d059fb938398b69bd4108aad6772f323
Frederik Schweiger <mail@flschweiger.net>
2020-05-22 16:04:57 +0300

    Add a comment describing the option functionality

--- lib/src/common/options/wiredash_options_data.dart
+++ lib/src/common/options/wiredash_options_data.dart
@@ -5,5 +5,7 @@ class WiredashOptionsData {
     bool showDebugFloatingEntryPoint,
   }) : showDebugFloatingEntryPoint = showDebugFloatingEntryPoint ?? kDebugMode;
 
+  /// Show a floating button with the Wiredash logo to easily report issues
+  /// while debugging the app
   final bool showDebugFloatingEntryPoint;
 }
