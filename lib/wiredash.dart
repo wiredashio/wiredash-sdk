82139fd6e99c81746fd1ca59a7c0b10beccadda4
Frederik Schweiger <mail@flschweiger.net>
2020-05-22 15:02:40 +0300

    Remove access to the floating entry widget

--- lib/wiredash.dart
+++ lib/wiredash.dart
@@ -3,6 +3,5 @@ library wiredash;
 export 'src/common/theme/wiredash_theme_data.dart';
 export 'src/common/translation/wiredash_translation_data.dart';
 export 'src/common/widgets/confidential.dart';
-export 'src/common/widgets/floating_entry.dart';
 export 'src/wiredash_controller.dart';
 export 'src/wiredash_widget.dart' show Wiredash;
