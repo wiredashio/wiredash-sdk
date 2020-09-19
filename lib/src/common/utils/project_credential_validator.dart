8ddaba1abd86fa2dac40185f1620f79e564401fb
Iiro Krankka <iiro.krankka@gmail.com>
2020-09-19 17:42:03 +0300

    Throw errors when using copy-pasted project credentials in debug mode.

---
+++ lib/src/common/utils/project_credential_validator.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/foundation.dart';
+
+/// Validates project credentials, such as project id and secret, and throws
+/// errors if they are invalid.
+///
+/// Does validation only in debug mode to prevent crashing production applications.
+class ProjectCredentialValidator {
+  const ProjectCredentialValidator();
+
+  Future<void> validate({
+    @required String projectId,
+    @required String secret,
+  }) async {
+    assert(() {
+      if (projectId == 'YOUR-PROJECT-ID') {
+        throw ArgumentError.value(
+          projectId,
+          'projectId',
+          "It seems like you forgot to add the projectId from your Wiredash console in your Wiredash widget.",
+        );
+      }
+
+      if (secret == 'YOUR-SECRET') {
+        throw ArgumentError.value(
+          secret,
+          'secret',
+          "It seems like you forgot to add the secret from your Wiredash console in your Wiredash widget.",
+        );
+      }
+
+      // TODO: In the future, send projectId & secret to the backend server for validation.
+      return true;
+    }());
+  }
+}
--- test/wiredash_widget_test.dart
+++ test/wiredash_widget_test.dart
@@ -1,22 +1,59 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:wiredash/src/common/utils/project_credential_validator.dart';
+import 'package:wiredash/src/wiredash_widget.dart';
 import 'package:wiredash/wiredash.dart';
 
+class MockProjectCredentialValidator extends Mock
+    implements ProjectCredentialValidator {}
+
 void main() {
-  testWidgets('Wiredash widget can be created', (WidgetTester tester) async {
-    const key = ValueKey('wiredash');
-    await tester.pumpWidget(
-      Wiredash(
-        projectId: 'test',
-        secret: 'test',
-        navigatorKey: GlobalKey<NavigatorState>(),
-        key: key,
-        child: const SizedBox(),
-      ),
+  group('Wiredash', () {
+    MockProjectCredentialValidator mockProjectCredentialValidator;
+
+    setUp(() {
+      mockProjectCredentialValidator = MockProjectCredentialValidator();
+      debugProjectCredentialValidator = mockProjectCredentialValidator;
+    });
+
+    tearDown(() {
+      debugProjectCredentialValidator = const ProjectCredentialValidator();
+    });
+
+    testWidgets('widget can be created', (tester) async {
+      await tester.pumpWidget(
+        Wiredash(
+          projectId: 'test',
+          secret: 'test',
+          navigatorKey: GlobalKey<NavigatorState>(),
+          child: const SizedBox(),
+        ),
+      );
+
+      expect(find.byType(Wiredash), findsOneWidget);
+    });
+
+    testWidgets(
+      'calls ProjectCredentialValidator.validate() initially',
+      (tester) async {
+        await tester.pumpWidget(
+          Wiredash(
+            projectId: 'my-project-id',
+            secret: 'my-secret',
+            navigatorKey: GlobalKey<NavigatorState>(),
+            child: const SizedBox(),
+          ),
+        );
+
+        verify(
+          mockProjectCredentialValidator.validate(
+            projectId: 'my-project-id',
+            secret: 'my-secret',
+          ),
+        );
+        verifyNoMoreInteractions(mockProjectCredentialValidator);
+      },
     );
-
-    final wiredashFinder = find.byKey(key);
-
-    expect(wiredashFinder, findsOneWidget);
   });
 }
