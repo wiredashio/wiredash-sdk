0c6500c8cbd2ca8d6db6a299b43f997ce27fffa0
Pascal Welsch <pascal@welsch.dev>
2021-07-19 01:18:46 +0200

    Split api and disk serialization tests

--- test/feedback/data/persisted_feedback_item_test.dart
+++ test/feedback/data/persisted_feedback_item_test.dart
@@ -98,40 +98,5 @@ void main() {
         },
       );
     });
-
-    test('toFeedbackBody()', () {
-      expect(
-        const PersistedFeedbackItem(
-          deviceInfo: DeviceInfo(
-            appIsDebug: true,
-            deviceId: "8F821AB6-B3A7-41BA-882E-32D8367243C1",
-            locale: "en_US",
-            padding: [0, 66, 0, 0],
-            physicalSize: [1080, 2088],
-            pixelRatio: 2.75,
-            platformOS: "android",
-            platformOSBuild: "RSR1.201013.001",
-            platformVersion:
-                '2.10.2 (stable) (Tue Oct 13 15:50:27 2020 +0200) on "android_ia32"',
-            textScaleFactor: 1,
-            viewInsets: [0, 0, 0, 685],
-          ),
-          email: 'email@example.com',
-          message: 'Hello world!',
-          type: 'bug',
-          user: 'Testy McTestFace',
-          sdkVersion: 1,
-        ).toFeedbackBody(),
-        {
-          'deviceInfo':
-              '{"appIsDebug":true,"deviceId":"8F821AB6-B3A7-41BA-882E-32D8367243C1","locale":"en_US","padding":[0.0,66.0,0.0,0.0],"physicalSize":[1080.0,2088.0],"pixelRatio":2.75,"platformOS":"android","platformOSBuild":"RSR1.201013.001","platformVersion":"2.10.2 (stable) (Tue Oct 13 15:50:27 2020 +0200) on \\"android_ia32\\"","textScaleFactor":1.0,"viewInsets":[0.0,0.0,0.0,685.0]}',
-          'email': 'email@example.com',
-          'message': 'Hello world!',
-          'type': 'bug',
-          'user': 'Testy McTestFace',
-          'sdkVersion': '1',
-        },
-      );
-    });
   });
 }
