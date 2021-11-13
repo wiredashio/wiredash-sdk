540d8c449c830f185298cde360201d848e6bddee
Frederik Schweiger <mail@flschweiger.net>
2021-11-13 12:55:25 +0100

    Add ScreencaptureController

--- lib/src/common/widgets/screencapture.dart
+++ lib/src/common/widgets/screencapture.dart
@@ -7,25 +7,50 @@ import 'package:flutter/widgets.dart';
 class Screencapture extends StatefulWidget {
   const Screencapture({
     Key? key,
+    required this.controller,
     required this.child,
   }) : super(key: key);
 
+  final ScreencaptureController controller;
   final Widget child;
 
   @override
-  ScreencaptureState createState() => ScreencaptureState();
+  _ScreencaptureState createState() => _ScreencaptureState();
 }
 
-class ScreencaptureState extends State<Screencapture> {
+class ScreencaptureController extends ChangeNotifier {
+  late _ScreencaptureState? _state;
+
+  ui.Image? get screenshot => _screenshot;
+  ui.Image? _screenshot;
+
+  Future<ui.Image?> captureScreen() async {
+    _screenshot = await _state!.captureScreen();
+    notifyListeners();
+    return _screenshot;
+  }
+
+  void releaseScreen() {
+    _state!.releaseScreen();
+  }
+}
+
+class _ScreencaptureState extends State<Screencapture> {
   final _repaintBoundaryGlobalKey = GlobalKey();
   MemoryImage? _screenshotMemoryImage;
+
+  @override
+  void initState() {
+    super.initState();
+    widget.controller._state = this;
+  }
 
   Future<ui.Image?> captureScreen() async {
     final canvas = _repaintBoundaryGlobalKey.currentContext?.findRenderObject()
         as RenderRepaintBoundary?;
     if (canvas == null) return null;
 
-    final _screenshot = await canvas.toImage(pixelRatio: 1.2);
+    final _screenshot = await canvas.toImage(pixelRatio: 1.5);
 
     final byteData =
         await _screenshot.toByteData(format: ui.ImageByteFormat.png);
