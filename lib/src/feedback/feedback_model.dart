b4bafa1bb73808eeb923b6649cd32a0faf8ee2af
Pascal Welsch <pascal@welsch.dev>
2022-01-28 12:18:21 +0100

    Draw the appBackgroundColor below the screenshot
    This is important for translucent apps, otherwise the background would be just black

--- lib/src/feedback/feedback_model.dart
+++ lib/src/feedback/feedback_model.dart
@@ -222,6 +222,8 @@ class FeedbackModel with ChangeNotifier {
 
         _screenshot = await _services.picassoController.paintDrawingOntoImage(
           _services.screenCaptureController.screenshot!,
+          _services.wiredashWidget.theme?.appBackgroundColor ??
+              const Color(0xffcccccc),
         );
         notifyListeners();
 
