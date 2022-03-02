e76552a7347fa16029ff830d99149e9630a1fd69
Pascal Welsch <pascal@welsch.dev>
2022-03-02 17:03:20 +0100

    Animate when deleting the last screenshot

--- lib/src/feedback/ui/steps/step_3_screenshot_overview.dart
+++ lib/src/feedback/ui/steps/step_3_screenshot_overview.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:wiredash/src/common/theme/wiredash_theme.dart';
 import 'package:wiredash/src/common/utils/color_ext.dart';
+import 'package:wiredash/src/common/widgets/animated_fade_widget_switcher.dart';
 import 'package:wiredash/src/common/widgets/tron_button.dart';
 import 'package:wiredash/src/common/widgets/tron_icon.dart';
 import 'package:wiredash/src/common/widgets/wirecons.dart';
@@ -21,10 +22,15 @@ class Step3ScreenshotOverview extends StatefulWidget {
 class _Step3ScreenshotOverviewState extends State<Step3ScreenshotOverview> {
   @override
   Widget build(BuildContext context) {
-    if (!context.feedbackModel.hasAttachments) {
-      return const Step3NotAttachments();
-    }
-    return const Step3WithGallery();
+    return AnimatedFadeWidgetSwitcher(
+      duration: const Duration(seconds: 1),
+      child: () {
+        if (!context.feedbackModel.hasAttachments) {
+          return const Step3NotAttachments();
+        }
+        return const Step3WithGallery();
+      }(),
+    );
   }
 }
 
