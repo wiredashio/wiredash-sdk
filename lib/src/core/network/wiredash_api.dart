019ce4d8579d7e50386f71a8df9a958593c2c280
Pascal Welsch <pascal@welsch.dev>
2022-04-22 01:37:50 +0200

    Switch to prod API

--- lib/src/core/network/wiredash_api.dart
+++ lib/src/core/network/wiredash_api.dart
@@ -28,9 +28,8 @@ class WiredashApi {
   final String _secret;
   final Future<String> Function() _deviceIdProvider;
 
-  // static const String _host = 'https://api.wiredash.io/sdk';
-
-  static const String _host = 'https://api.wiredash.dev/sdk';
+  static const String _host = 'https://api.wiredash.io/sdk';
+  // static const String _host = 'https://api.wiredash.dev/sdk';
 
   /// Uploads a attachment to the Wiredash hosting service
   ///
