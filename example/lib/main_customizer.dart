92673877a1847935afa88f8e69610f43ac037490
Pascal Welsch <pascal@welsch.dev>
2022-04-25 18:08:39 +0200

    Style background colors

--- example/lib/main_customizer.dart
+++ example/lib/main_customizer.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_colorpicker/flutter_colorpicker.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:wiredash/wiredash.dart';
 import 'package:wiredash_example/marianos_clones/whatsapp_clone.dart';
 
@@ -96,6 +97,25 @@ class ThemeModel extends ChangeNotifier {
     notifyListeners();
   }
 
+  Color _primaryBackgroundColor = WiredashThemeData().primaryBackgroundColor;
+
+  Color get primaryBackgroundColor => _primaryBackgroundColor;
+
+  set primaryBackgroundColor(Color primaryBackgroundCColor) {
+    _primaryBackgroundColor = primaryBackgroundCColor;
+    notifyListeners();
+  }
+
+  Color _secondaryBackgroundColor =
+      WiredashThemeData().secondaryBackgroundColor;
+
+  Color get secondaryBackgroundColor => _secondaryBackgroundColor;
+
+  set secondaryBackgroundColor(Color secondaryBackgroundColor) {
+    _secondaryBackgroundColor = secondaryBackgroundColor;
+    notifyListeners();
+  }
+
   static ThemeModel of(BuildContext context, {bool listen = true}) {
     if (listen) {
       return context
@@ -123,47 +143,135 @@ extension on BuildContext {
   ThemeModel get readThemeModel => ThemeModel.of(this, listen: false);
 }
 
-class ThemeControls extends StatelessWidget {
+class ThemeControls extends StatefulWidget {
   const ThemeControls({Key? key}) : super(key: key);
 
   @override
+  State<ThemeControls> createState() => _ThemeControlsState();
+}
+
+class _ThemeControlsState extends State<ThemeControls> {
+  @override
   Widget build(BuildContext context) {
+    _entry.markNeedsBuild();
     return Overlay(
-      initialEntries: [
-        OverlayEntry(
-          builder: (context) {
-            return Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text('Primary Color'),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 8.0),
-                    child: WiredashColorPicker(
-                      color: context.watchThemeModel.primaryColor,
-                      onColorChanged: (color) {
-                        context.readThemeModel.primaryColor = color;
-                      },
-                    ),
+      initialEntries: [_entry],
+    );
+  }
+
+  late final _entry = OverlayEntry(builder: _buildEntryContent);
+
+  Widget _buildEntryContent(BuildContext context) {
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SelectableText('Base Colors', style: TextStyle(fontSize: 20)),
+            SizedBox(height: 20),
+            Wrap(
+              spacing: 20,
+              children: [
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SelectableText(
+                        'primaryColor',
+                        style: GoogleFonts.droidSansMono(),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 8.0),
+                        child: WiredashColorPicker(
+                          color: context.watchThemeModel.primaryColor,
+                          onColorChanged: (color) {
+                            context.readThemeModel.primaryColor = color;
+                          },
+                        ),
+                      ),
+                    ],
                   ),
-                  SizedBox(height: 20),
-                  Text('Secondary Color'),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 8.0),
-                    child: WiredashColorPicker(
-                      color: context.watchThemeModel.secondaryColor,
-                      onColorChanged: (color) {
-                        context.readThemeModel.secondaryColor = color;
-                      },
-                    ),
+                ),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SelectableText(
+                        'secondaryColor',
+                        style: GoogleFonts.droidSansMono(),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 8.0),
+                        child: WiredashColorPicker(
+                          color: context.watchThemeModel.secondaryColor,
+                          onColorChanged: (color) {
+                            context.readThemeModel.secondaryColor = color;
+                          },
+                        ),
+                      ),
+                    ],
                   ),
-                ],
-              ),
-            );
-          },
+                ),
+              ],
+            ),
+            SizedBox(height: 40),
+            SelectableText('Background Colors', style: TextStyle(fontSize: 20)),
+            SizedBox(height: 20),
+            Wrap(
+              spacing: 20,
+              children: [
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SelectableText(
+                        'primaryBackgroundColor',
+                        style: GoogleFonts.droidSansMono(),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 8.0),
+                        child: WiredashColorPicker(
+                          withAlpha: true,
+                          color: context.watchThemeModel.primaryBackgroundColor,
+                          onColorChanged: (color) {
+                            context.readThemeModel.primaryBackgroundColor =
+                                color;
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SelectableText(
+                        'secondaryBackgroundColor',
+                        style: GoogleFonts.droidSansMono(),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 8.0),
+                        child: WiredashColorPicker(
+                          withAlpha: true,
+                          color:
+                              context.watchThemeModel.secondaryBackgroundColor,
+                          onColorChanged: (color) {
+                            context.readThemeModel.secondaryBackgroundColor =
+                                color;
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+          ],
         ),
-      ],
+      ),
     );
   }
 }
@@ -173,10 +281,12 @@ class WiredashColorPicker extends StatefulWidget {
     Key? key,
     required this.color,
     required this.onColorChanged,
+    this.withAlpha = false,
   }) : super(key: key);
 
   final Color color;
   final void Function(Color) onColorChanged;
+  final bool withAlpha;
 
   @override
   State<WiredashColorPicker> createState() => _WiredashColorPickerState();
@@ -215,16 +325,28 @@ class _WiredashColorPickerState extends State<WiredashColorPicker> {
                 SizedBox(
                   height: 32,
                   width: 50,
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(4),
-                    child: Material(
-                      color: widget.color,
+                  child: Material(
+                    elevation: 5,
+                    color: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(4),
+                    ),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(4),
                       child: InkWell(
                         onTap: () {
                           openColorPicker();
                         },
-                        highlightColor: widget.color.withAlpha(20),
-                        child: Container(),
+                        child: Stack(
+                          children: [
+                            Positioned.fill(
+                              child: const CustomPaint(
+                                painter: CheckerPainter(),
+                              ),
+                            ),
+                            Container(color: widget.color),
+                          ],
+                        ),
                       ),
                     ),
                   ),
@@ -287,7 +409,7 @@ class _WiredashColorPickerState extends State<WiredashColorPicker> {
                         colorPickerWidth: 200,
                         portraitOnly: true,
                         pickerAreaHeightPercent: 0.7,
-                        enableAlpha: false,
+                        enableAlpha: widget.withAlpha,
                         displayThumbColor: true,
                         hexInputController: _textEditingController,
                         pickerColor: widget.color,
@@ -353,6 +475,10 @@ class DeviceFrame extends StatelessWidget {
                       theme: WiredashThemeData(
                         primaryColor: context.watchThemeModel.primaryColor,
                         secondaryColor: context.watchThemeModel.secondaryColor,
+                        primaryBackgroundColor:
+                            context.watchThemeModel.primaryBackgroundColor,
+                        secondaryBackgroundColor:
+                            context.watchThemeModel.secondaryBackgroundColor,
                       ),
                       child: child,
                     ),
