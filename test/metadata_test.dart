305d8fc57df145d5a9a04c3f73e31f62bdfa1cfc
Pascal Welsch <pascal@welsch.dev>
2022-07-04 19:38:21 +0200

    Cleanup

--- test/metadata_test.dart
+++ test/metadata_test.dart
@@ -1,16 +1,9 @@
 // ignore_for_file: avoid_print
 
-import 'dart:async';
-
 import 'package:flutter_test/flutter_test.dart';
 import 'package:wiredash/src/_wiredash_internal.dart';
-import 'package:wiredash/src/core/options/feedback_options.dart';
-import 'package:wiredash/src/core/widgets/larry_page_view.dart';
-import 'package:wiredash/src/feedback/_feedback.dart';
 
-import 'util/assert_widget.dart';
 import 'util/robot.dart';
-import 'util/wiredash_tester.dart';
 
 void main() {
   test('userEmail can be set', () async {
@@ -53,7 +46,7 @@ void main() {
     final WiredashModel model = WiredashModel(createMockServices());
     final controller = WiredashController(model);
     final map = controller.metaData.custom;
-    expect(() => map['foo'] = 'bar', throws);
+    expect(() => map['foo'] = 'bar', throwsA(isA<UnsupportedError>()));
   });
 
   test('custom metadata can be mutated in modifyMetaData', () async {
