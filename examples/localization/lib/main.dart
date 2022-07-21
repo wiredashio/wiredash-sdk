b2416e47799d5d6340f16775d3340c544ba43ee7
Pascal Welsch <pascal@welsch.dev>
2022-07-21 19:18:05 +0200

    Add PL as custom localization

--- examples/localization/lib/main.dart
+++ examples/localization/lib/main.dart
@@ -19,6 +19,7 @@ class LocalizationExample extends StatefulWidget {
 }
 
 class _LocalizationExampleState extends State<LocalizationExample> {
+  /// The locale that was selected by the user, defaults to the system locale
   Locale _selectedLocale = window.locale;
 
   @override
@@ -28,6 +29,9 @@ class _LocalizationExampleState extends State<LocalizationExample> {
       secret: "API Key from console.wiredash.io",
       options: WiredashOptionsData(
         locale: _selectedLocale,
+        // You can override all existing text with a delegate
+        // The same way you can add additional languages
+        localizationDelegate: const CustomWiredashTranslationsDelegate(),
       ),
       child: MaterialApp(
         locale: _selectedLocale,
@@ -36,6 +40,7 @@ class _LocalizationExampleState extends State<LocalizationExample> {
           Locale('en'),
           Locale('de'),
           Locale('pl'),
+          Locale('ko'),
         ],
         localizationsDelegates: [
           GlobalMaterialLocalizations.delegate,
@@ -71,7 +76,13 @@ class _LocalizationExampleState extends State<LocalizationExample> {
             children: [
               SizedBox(height: 20),
               Text("System locale: ${window.locale}"),
+
+              // When user selects a locale that's not in listed in
+              // supportedLocales then MaterialApp uses a fallback
               Text("Selected locale: $_selectedLocale"),
+
+              // The Locale MaterialApp has chosed from the supportedLocales
+              // see https://api.flutter.dev/flutter/widgets/basicLocaleListResolution.html
               Text("Applied locale: ${Localizations.localeOf(context)}"),
               SizedBox(height: 20),
               SizedBox(height: 20),
@@ -100,6 +111,22 @@ class _LocalizationExampleState extends State<LocalizationExample> {
                   ElevatedButton(
                     onPressed: () {
                       setState(() {
+                        _selectedLocale = Locale('pl', 'PL');
+                      });
+                    },
+                    child: Text('pl_PL'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        _selectedLocale = Locale('ko', 'KR');
+                      });
+                    },
+                    child: Text('ko_KR'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      setState(() {
                         _selectedLocale = window.locale;
                       });
                     },
@@ -116,31 +143,73 @@ class _LocalizationExampleState extends State<LocalizationExample> {
   }
 }
 
+/// Overrides existing and adds new Wiredash locales
 class CustomWiredashTranslationsDelegate
     extends LocalizationsDelegate<WiredashLocalizations> {
   const CustomWiredashTranslationsDelegate();
 
   @override
-  bool isSupported(Locale locale) => locale.languageCode == 'en';
+  bool isSupported(Locale locale) {
+    return ['en', 'de', 'pl'].contains(locale.languageCode);
+  }
 
   @override
-  Future<WiredashLocalizations> load(Locale locale) =>
-      SynchronousFuture(_CustomTranslationsEn());
+  Future<WiredashLocalizations> load(Locale locale) {
+    switch (locale.languageCode) {
+      case 'en':
+        return SynchronousFuture(_EnOverrides());
+      case 'de':
+        return SynchronousFuture(_DeOverrides());
+      case 'pl':
+        return SynchronousFuture(_WiredashLocalizationsPl());
+      default:
+        throw "Unsupported locale $locale";
+    }
+  }
 
   @override
   bool shouldReload(CustomWiredashTranslationsDelegate old) => false;
 }
 
-/// This english translation extends the default english Wiredash translations.
-/// This makes is robost to changes when new terms are added.
-class _CustomTranslationsEn extends WiredashLocalizationsEn {
+/// This is an override of the Wiredash EN translations.
+///
+/// Use extends instead of implements to make it robost to changes when new terms are added.
+class _EnOverrides extends WiredashLocalizationsEn {
   @override
-  String get feedbackStep1MessageTitle => 'Custom feedbackStep1MessageTitle';
+  String get feedbackStep1MessageTitle => 'Do you have a problem?';
 
   @override
   String get feedbackStep1MessageDescription =>
-      'Custom feedbackStep1MessageDescription';
+      "Don't hesitate to send us your honest feedback. Crashes, bugs, and other issues are welcome.";
 
   @override
-  String get feedbackStep1MessageHint => 'Custom feedbackStep1MessageHint';
+  String get feedbackStep1MessageHint => '...';
+}
+
+class _DeOverrides extends WiredashLocalizationsDe {
+  @override
+  String get feedbackStep1MessageTitle => 'Kunden Feedback Formular';
+
+  @override
+  String get feedbackStep1MessageDescription =>
+      "Verehrter Nutzer,\nbitte senden Sie uns Ihre Erfahrungen mit diesem Produkt. Gut oder schlecht, wir versuchen aufgrund Ihres Feedbacks unser Produkt ständig wetierzuentwickeln und zu verbessern.\n\nHochachtungsvoll,\nIhr Produkt Team";
+
+  @override
+  String get feedbackStep1MessageHint =>
+      'Ich kann die Fax Nummer für die Online-Registrierung nicht finden.';
+}
+
+/// In case Wiredash doesn't support your locale, you can add it on your own.
+///
+/// Consider contributing back to wiredash!
+class _WiredashLocalizationsPl extends WiredashLocalizationsEn {
+  @override
+  String get feedbackStep1MessageTitle => 'Czołem';
+
+  @override
+  String get feedbackStep1MessageDescription =>
+      "Czytamy uważnie wszystkie opinie. Podaj jak najwięcej szczegółów.";
+
+  @override
+  String get feedbackStep1MessageHint => 'Twój feedback';
 }
