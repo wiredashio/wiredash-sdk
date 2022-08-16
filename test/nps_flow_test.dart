0a2cd791c4582fb5744d7a4ba2369c278e8c160e
Pascal Welsch <pascal@welsch.dev>
2022-08-16 22:39:42 +0200

    Test that 3 calls are sent

--- test/nps_flow_test.dart
+++ test/nps_flow_test.dart
@@ -4,6 +4,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:wiredash/src/_wiredash_internal.dart';
 import 'package:wiredash/src/nps/nps_model.dart';
 
+import 'util/invocation_catcher.dart';
 import 'util/robot.dart';
 
 void main() {
@@ -22,9 +23,61 @@ void main() {
       await tester.pump(const Duration(milliseconds: 900));
       await robot.showsNpsThanksMessage();
       await robot.waitUntilWiredashIsClosed();
+      final npsCalls =
+          robot.mockServices.mockApi.sendNpsInvocations.invocations;
+      expect(npsCalls, hasLength(3));
+      final callOne = npsCalls[0][0] as NpsRequestBody?;
+      expect(callOne!.score, isNull);
+      expect(callOne.platformOS, isNotNull);
+      expect(callOne.message, isNull);
+
+      final callTwo = npsCalls[1][0] as NpsRequestBody?;
+      expect(callTwo!.score?.intValue, 7);
+      expect(callTwo.platformOS, isNotNull);
+      expect(callTwo.message, isNull);
+
+      final callThree = npsCalls[2][0] as NpsRequestBody?;
+      expect(callThree!.score?.intValue, 7);
+      expect(callThree.platformOS, isNotNull);
+      expect(callThree.message, isNull);
+    });
+
+    testWidgets('Send NPS with message', (tester) async {
+      final robot = await WiredashTestRobot.launchApp(tester);
+      await robot.openNps();
+      await robot.rateNps(3);
+      await robot.enterNpsMessage('What a cool app!');
+      await robot.submitNps();
+      await robot.showsNpsThanksMessage();
+      // Shows success message for a while
+      await tester.pump(const Duration(milliseconds: 900));
+      await robot.showsNpsThanksMessage();
+      await robot.waitUntilWiredashIsClosed();
+      final npsCalls =
+          robot.mockServices.mockApi.sendNpsInvocations.invocations;
+      expect(npsCalls, hasLength(3));
+      final callOne = npsCalls[0][0] as NpsRequestBody?;
+      expect(callOne!.score, isNull);
+      expect(callOne.platformOS, isNotNull);
+      expect(callOne.message, isNull);
+
+      final callTwo = npsCalls[1][0] as NpsRequestBody?;
+      expect(callTwo!.score?.intValue, 3);
+      expect(callTwo.platformOS, isNotNull);
+      expect(callTwo.message, isNull);
+
+      final callThree = npsCalls[2][0] as NpsRequestBody?;
+      expect(callThree!.score?.intValue, 3);
+      expect(callThree.platformOS, isNotNull);
+      expect(callThree.message, 'What a cool app!');
+    });
+
+    testWidgets('Send NPS shown to console', (tester) async {
+      final robot = await WiredashTestRobot.launchApp(tester);
+      await robot.openNps();
       final latestCall = robot.mockServices.mockApi.sendNpsInvocations.latest;
       final request = latestCall[0] as NpsRequestBody?;
-      expect(request!.score?.intValue, 7);
+      expect(request!.deviceId, isNotNull);
     });
 
     testWidgets('Shows detractors thanks message', (tester) async {
