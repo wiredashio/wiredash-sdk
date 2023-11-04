dbd56ed41e109413acf3d00c7078fbd48810e5d7
Pascal Welsch <pascal@welsch.dev>
2023-11-04 23:35:33 +0100

    Test offline upload of v3 feedback

--- test/offline_feedback_test.dart
+++ test/offline_feedback_test.dart
@@ -46,7 +46,51 @@ void main() {
       final latestFeedbackCall =
           robot.mockServices.mockApi.sendFeedbackInvocations.latest;
       final submittedFeedback = latestFeedbackCall[0] as FeedbackItem?;
-      expect(submittedFeedback!.message, 'Pending Feedback Item');
+      expect(submittedFeedback!.message, 'Pending Feedback Item v2');
+      expect(uploadedAttachment, isNotNull);
+      expect(
+        submittedFeedback.attachments![0].file.attachmentId,
+        uploadedAttachment,
+      );
+    });
+
+    testWidgets('v3 feedback upload', (tester) async {
+      final robot = WiredashTestRobot(tester);
+      robot.setupMocks();
+
+      // insert feedback in v2 format with attachment
+      final tempDir = Directory.systemTemp.createTempSync();
+      File('${tempDir.path}/image.png').writeAsStringSync('test img content');
+      final json = fullJsonV3;
+      // ignore: avoid_dynamic_calls
+      json['feedbackItem']['attachments'][0]['path'] =
+          '${tempDir.path}/image.png';
+      final prefs = await SharedPreferences.getInstance();
+      await prefs.setStringList(
+        'io.wiredash.pending_feedback_items',
+        [jsonEncode(json)],
+      );
+
+      await robot.launchApp(useDirectFeedbackSubmitter: false);
+
+      AttachmentId? uploadedAttachment;
+      robot.mockServices.mockApi.uploadAttachmentInvocations.interceptor =
+          (call) async {
+        final name = call.namedArguments[#filename] as String;
+        return uploadedAttachment = AttachmentId(name);
+      };
+
+      // wait for the UploadPendingFeedbackJob to start
+      await tester.pumpAndSettle(const Duration(seconds: 5));
+      // every disk io call needs to be pumped
+      for (var i = 0; i < 10; i++) {
+        await tester.pumpHardAndSettle();
+      }
+
+      final latestFeedbackCall =
+          robot.mockServices.mockApi.sendFeedbackInvocations.latest;
+      final submittedFeedback = latestFeedbackCall[0] as FeedbackItem?;
+      expect(submittedFeedback!.message, 'Pending Feedback Item v3');
       expect(uploadedAttachment, isNotNull);
       expect(
         submittedFeedback.attachments![0].file.attachmentId,
@@ -118,9 +162,53 @@ Map get fullJsonV2 => {
         },
         'deviceId': '8F821AB6-B3A7-41BA-882E-32D8367243C1',
         'email': 'email@example.com',
-        'message': 'Pending Feedback Item',
+        'message': 'Pending Feedback Item v2',
         'labels': ['bug', 'lbl-1234'],
         'userId': 'Testy McTestFace',
         'sdkVersion': 174,
       },
+    };
+
+Map get fullJsonV3 => {
+      "feedbackItem": {
+        "attachments": [
+          {"path": "ATTACHMENT_PATH"},
+        ],
+        "labels": ["bug", "lbl-1234"],
+        "message": "Pending Feedback Item v3",
+        "metadata": {
+          "appLocale": "en_US",
+          "appName": "MyApp",
+          "buildCommit": "abcdef12",
+          "buildNumber": "543",
+          "buildVersion": "1.2.3",
+          "bundleId": "com.example.app",
+          "compilationMode": "profile",
+          "custom": {
+            "customText": "text",
+            "nestedObject": {"frodo": "ring", "sam": "lembas"},
+          },
+          "deviceModel": "Google Pixel 8",
+          "installId": "8F821AB6-B3A7-41BA-882E-32D8367243C1",
+          "physicalGeometry": [0.0, 0.0, 0.0, 0.0],
+          "platformBrightness": "dark",
+          "platformDartVersion":
+              '2.10.2 (stable) (Tue Oct 13 15:50:27 2020 +0200) on "android_ia32"',
+          "platformGestureInsets": [0.0, 0.0, 0.0, 0.0],
+          "platformLocale": "en_US",
+          "platformOS": "android",
+          "platformOSVersion": "RSR1.201013.001",
+          "platformSupportedLocales": ["en_US", "de_DE"],
+          "sdkVersion": 174,
+          "userEmail": "email@example.com",
+          "userId": "Testy McTestFace",
+          "windowInsets": [0.0, 0.0, 0.0, 685.0],
+          "windowPadding": [0.0, 66.0, 0.0, 0.0],
+          "windowPixelRatio": 2.75,
+          "windowSize": [1280.0, 720.0],
+          "windowTextScaleFactor": 1.0,
+        },
+      },
+      "id": "abc123",
+      "version": 3,
     };
