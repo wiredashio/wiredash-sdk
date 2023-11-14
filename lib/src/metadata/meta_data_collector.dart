c61d24f7fa2dfed3fb59670bba2d0b795527b4f9
Pascal Welsch <pascal@welsch.dev>
2023-11-14 01:03:08 +0100

    Ignore being unable to read device info on linux
    related: https://github.com/fluttercommunity/plus_plugins/issues/1552

--- lib/src/metadata/meta_data_collector.dart
+++ lib/src/metadata/meta_data_collector.dart
@@ -179,6 +179,14 @@ class MetaDataCollector {
 
       // there's not way to get the model of windows or linux devices
     } catch (e, stack) {
+      const issue1552 =
+          "type 'BaseDeviceInfo' is not a subtype of type 'LinuxDeviceInfo' in type cast";
+      if (e.toString().contains(issue1552)) {
+        // ignore, will be fixed in an upcoming device_info_plus release
+        // https://github.com/fluttercommunity/plus_plugins/issues/1552
+        return const DeviceInfo();
+      }
+
       reportWiredashError(
         e,
         stack,
