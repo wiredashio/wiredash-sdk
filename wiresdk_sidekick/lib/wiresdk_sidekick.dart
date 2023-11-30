c6e896fa96e6f420d2105735b5cdf83a202e0ea2
Pascal Welsch <pascal@welsch.dev>
2023-11-30 17:21:22 +0100

    Add wiresdk bump-version

--- wiresdk_sidekick/lib/wiresdk_sidekick.dart
+++ wiresdk_sidekick/lib/wiresdk_sidekick.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:phntmxyz_bump_version_sidekick_plugin/phntmxyz_bump_version_sidekick_plugin.dart';
 import 'package:sidekick_core/sidekick_core.dart';
 import 'package:wiresdk_sidekick/src/commands/clean_command.dart';
 import 'package:wiresdk_sidekick/src/commands/gen_l10n_command.dart';
@@ -22,7 +23,12 @@ Future<void> runWiresdk(List<String> args) async {
     ..addCommand(SidekickCommand())
     ..addCommand(SyncBranchesCommand())
     ..addCommand(GenL10nCommand())
-    ..addCommand(TestCommand());
+    ..addCommand(TestCommand())
+    ..addCommand(
+      BumpVersionCommand()
+        ..addModification(bumpVersionFile)
+        ..addModification(bumpReadme),
+    );
 
   try {
     return await runner.run(args);
@@ -30,4 +36,53 @@ Future<void> runWiresdk(List<String> args) async {
     print(e);
     exit(64); // usage error
   }
+}
+
+Future<void> bumpVersionFile(
+  DartPackage package,
+  Version oldVersion,
+  Version newVersion,
+) async {
+  if (package.name != 'wiredash') return;
+
+  final versionFile = package.root.file('lib/src/core/version.dart');
+  final content = versionFile.readAsStringSync();
+
+  final versionNumberRegex = RegExp(r'const wiredashSdkVersion = (\d)+;');
+  final oldVersionNumber =
+      int.parse(versionNumberRegex.firstMatch(content)!.group(1)!);
+
+  int nextVersionNumber =
+      newVersion.major * 100 + newVersion.minor * 10 + newVersion.patch;
+
+  if (nextVersionNumber == oldVersionNumber) {
+    nextVersionNumber++;
+  }
+  if (nextVersionNumber < oldVersionNumber) {
+    throw 'New version number $nextVersionNumber for $newVersion is smaller '
+        'than the old version number $oldVersionNumber for $oldVersion';
+  }
+
+  final newContent = content.replaceAll(
+    versionNumberRegex,
+    "/// $nextVersionNumber -> $newVersion\n"
+    "const wiredashSdkVersion = $nextVersionNumber;",
+  );
+  versionFile.writeAsStringSync(newContent);
+}
+
+Future<void> bumpReadme(
+  DartPackage package,
+  Version oldVersion,
+  Version newVersion,
+) async {
+  final readme = package.root.file('README.md');
+  final content = readme.readAsStringSync();
+
+  final versionRegex = RegExp(r'wiredash: \^(.+)');
+  final update = content.replaceFirst(
+    versionRegex,
+    'wiredash: ^${newVersion.major}.${newVersion.minor}.0',
+  );
+  readme.writeAsStringSync(update);
 }
