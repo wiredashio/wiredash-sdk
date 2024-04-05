b2ae1857f54a880be3eedf9e5bb7664f622d6845
Pascal Welsch <pascal@wiredash.com>
2024-04-05 13:34:01 +0200

    Add warning when LocalizationsDelegate.load() is async
    Fixes https://github.com/wiredashio/wiredash-sdk/issues/341

--- test/util/flutter_error.dart
+++ test/util/flutter_error.dart
@@ -4,21 +4,16 @@ import 'package:flutter_test/flutter_test.dart';
 /// Consumes all [FlutterError.onError] and [FlutterError.presentError] calls
 /// during this test and makes them accessible as list for assertions.
 FlutterErrors captureFlutterErrors() {
-  final errors = FlutterErrors();
-  final oldPresentHandler = FlutterError.presentError;
+  final errors = FlutterErrors(FlutterError.onError, FlutterError.presentError);
   FlutterError.presentError = (details) {
     errors._presentError.add(details);
   };
-  addTearDown(() {
-    FlutterError.presentError = oldPresentHandler;
-  });
-
-  final oldOnErrorHandler = FlutterError.onError;
   FlutterError.onError = (FlutterErrorDetails details) {
     errors._onError.add(details);
   };
+
   addTearDown(() {
-    FlutterError.onError = oldOnErrorHandler;
+    errors.restoreDefaultErrorHandlers();
   });
 
   return errors;
@@ -26,10 +21,30 @@ FlutterErrors captureFlutterErrors() {
 
 /// A summary of [FlutterError.onError] and [FlutterError.presentError] calls
 class FlutterErrors {
+  final FlutterExceptionHandler? _originalOnError;
+  final FlutterExceptionHandler _originalPresentError;
+
+  FlutterErrors(
+    this._originalOnError,
+    this._originalPresentError,
+  );
+
   List<FlutterErrorDetails> get onError => List.unmodifiable(_onError);
   final List<FlutterErrorDetails> _onError = [];
 
   List<FlutterErrorDetails> get presentError =>
       List.unmodifiable(_presentError);
   final List<FlutterErrorDetails> _presentError = [];
+
+  void restoreDefaultErrorHandlers() {
+    FlutterError.onError = _originalOnError;
+    FlutterError.presentError = _originalPresentError;
+  }
+
+  String get presentErrorText {
+    final renderer = TextTreeRenderer(wrapWidth: 100000);
+    return presentError.map((e) {
+      return renderer.render(e.toDiagnosticsNode());
+    }).join('\n');
+  }
 }
