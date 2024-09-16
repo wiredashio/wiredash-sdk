ab27233e31ea8b22a254ba914b3f78873805dd3c
Pascal Welsch <pascal@wiredash.com>
2024-09-17 01:14:32 +0200

    Update examples to Flutter 3.24

--- examples/localization/android/app/src/main/kotlin/io/wiredash/example/wiredash_localization/MainActivity.kt
+++ examples/localization/android/app/src/main/kotlin/io/wiredash/example/wiredash_localization/MainActivity.kt
@@ -2,5 +2,4 @@ package io.wiredash.example.wiredash_localization
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity: FlutterActivity()
--- examples/theme_editor/android/app/src/main/kotlin/io/wiredash/example/wiredash_theme_editor/MainActivity.kt
+++ examples/theme_editor/android/app/src/main/kotlin/io/wiredash/example/wiredash_theme_editor/MainActivity.kt
@@ -2,5 +2,4 @@ package io.wiredash.example.wiredash_theme_editor
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity: FlutterActivity()
