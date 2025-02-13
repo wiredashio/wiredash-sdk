d78791f5c772a201252026f81e13f02da9f2bc8c
Pascal Welsch <pascal@wiredash.com>
2025-02-13 15:26:21 +0100

    Report if user submitted feedback, or not (#379)
    Fixes https://github.com/wiredashio/wiredash-sdk/issues/374

--- lib/wiredash.dart
+++ lib/wiredash.dart
@@ -18,5 +18,6 @@ export 'src/core/wiredash_controller.dart'
 export 'src/core/wiredash_widget.dart' show Wiredash;
 export 'src/feedback/data/label.dart';
 export 'src/feedback/feedback_options.dart';
+export 'src/feedback/feedback_result.dart';
 export 'src/metadata/user_meta_data.dart';
 export 'src/promoterscore/ps_options.dart';
